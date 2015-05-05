660b8c7de2f09d5fa9063a7012188bbac6051327
Fred Bradley <hello@fredbradley.co.uk>
2015-05-05 18:23:25 +0100

    added get station

--- controllers/controller.stations.php
+++ controllers/controller.stations.php
@@ -2,13 +2,35 @@
 
 class json_api_stations_controller {
 
-  public function hello_world() {
-	  global $json_api;
-	  //$json_api->error("You must include a 'cookie' authentication cookie. Use the `create_auth_cookie` Auth API method.");
+	public function hello_world() {
+		global $json_api;
+		//$json_api->error("You must include a 'cookie' authentication cookie. Use the `create_auth_cookie` Auth API method.");
 
-    return array(
-      "message" => "Hello, world, you are".get_current_user_id()
-    );
-  }
+		return array(
+			"message" => "Hello, world, you are".get_current_user_id()
+		);
+	}
+	
+	public function get_station() {
+		global $json_api;
+		extract($json_api->query->get(array('id', 'slug')));
+		
+		if ($id) {
+			$post = get_post($id);
+			return array(
+				"status" => "ok",
+				"station" => $post
+			);
+		} elseif ($slug) {
+			$args = array(
+				"post_type" => "station",
+				"page_name" => $slug
+			);
+			$query = new WP_Query($args);
+		} else {
+			$json_api->error("You must include either an 'id' or a 'slug' variable.");
+		}
 
+	}
+	
 }
